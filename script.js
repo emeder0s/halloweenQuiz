7c38b5b473e116dc6cc4262c386e374ab59a303a
Elena Mederos <elenamc91@hotmail.com>
2022-10-13 16:58:11 +0200

    Añadidos los ficheros y añadido esqueleto de la web

---
+++ script.js
@@ -0,0 +1,28 @@
+var usuario = {
+    "nombreUsuario": [
+        {"partidas": [{"Peliculas": "10"},{"Todas las caterias": "5"}]}
+    ]
+}
+
+var usuarios = [...usuario];
+
+//Comprueba si el usuario ya está registrado. 
+function existeUsuario(){
+
+}
+
+function registrarUsuario(){
+
+}
+
+function comprobarRespuesta(respuesta){
+
+}
+
+function jugar(){
+
+}
+
+
+
+// window.href= question.html
