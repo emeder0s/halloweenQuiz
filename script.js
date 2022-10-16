df137773f5e9d559aec772cf85104c4c28623d99
Elena Mederos <elenamc91@hotmail.com>
2022-10-16 22:21:16 +0200

    Añadido estilo en la cabecera h2 de las preguntas

--- script.js
+++ script.js
@@ -198,3 +198,25 @@ function registrarUsuario(){
         parrafo.style.display="";
     }
 }
+
+//CÓDIGO PARA CREAR EL EFECTO SMOKE
+const filter = document.querySelector("#turbulence");
+let frames = 1;
+let rad = Math.PI / 180;
+let bfx, bfy;
+
+function freqAnimation() {
+    frames += .2;
+    bfx = 0.03;
+    bfy = 0.03;
+
+    bfx += 0.005 * Math.cos(frames * rad);
+    bfy += 0.005 * Math.sin(frames * rad);
+
+    bf = `${String(bfx)} ${String(bfy)}`;
+    filter.setAttributeNS(null, "baseFrequency", bf);
+
+    requestAnimationFrame(freqAnimation);
+}
+
+requestAnimationFrame(freqAnimation);
